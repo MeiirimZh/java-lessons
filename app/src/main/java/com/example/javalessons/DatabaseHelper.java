9de6ed007d8c2b84c9a37a13d8d668b1747d2080
MeiirimZh <zh.meyirim@gmail.com>
2025-11-08 08:14:01 +0500

    created DatabaseHelper class

---
+++ app/src/main/java/com/example/javalessons/DatabaseHelper.java
@@ -0,0 +1,60 @@
+package com.example.javalessons;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "database.db";
+    private static final int DATABASE_VERSION = 1;
+    private final Context context;
+    private final String databasePath;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+        this.databasePath = context.getDatabasePath(DATABASE_NAME).getPath();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void createDatabaseIfNotExists() {
+        File dbFile = new File(databasePath);
+        if (!dbFile.exists()) {
+            dbFile.getParentFile().mkdirs();
+            copyDatabaseFromAssets();
+        }
+    }
+
+    private void copyDatabaseFromAssets() {
+        try {
+            InputStream inputStream = context.getAssets().open("databases/" + DATABASE_NAME);
+            OutputStream outputStream = new FileOutputStream(databasePath);
+
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, length);
+            }
+
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
