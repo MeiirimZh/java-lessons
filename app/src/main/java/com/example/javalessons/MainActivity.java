f0683018c5822e1f614ae720ed4ebcb9152aa6e6
MeiirimZh <zh.meyirim@gmail.com>
2025-11-10 11:09:21 +0500

    added navigation menu and fragments

--- app/src/main/java/com/example/javalessons/MainActivity.java
+++ app/src/main/java/com/example/javalessons/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.javalessons;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import com.example.javalessons.R;
+
+import com.example.javalessons.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.home) {
+                replaceFragment(new HomeFragment());
+            }
+            else if (id == R.id.lectures) {
+                replaceFragment(new LecturesFragment());
+            }
+            else {
+                replaceFragment(new TestsFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
